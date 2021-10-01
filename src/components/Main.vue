df8e16ac3633a57d5c7f010a2a5502fc0d4b49cc
andmalt <andrea.maltempi@gmail.com>
2021-10-01 21:31:41 +0200

    inserted methods search

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,8 +8,8 @@
           </div>
 
           <div class="col-6 d-flex justify-content-center align-items-center">
-            <input type="text" placeholder="Search">
-            <button class="btn-primary">search</button>
+            <input type="text" placeholder="Search" v-model="searchBar">
+            <button @click="search" class="btn-primary">search</button>
           </div>
         </div>
       </div>
@@ -22,10 +22,32 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Main',
   props: {
-  }
+  },
+  data(){
+    return{
+      searchBar:'',
+      results:[],
+    }
+  },
+  methods:{
+    search(){
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=9d0595ce1c1db683468e3d616a3c1b98&query=lo+squalo')
+        .then(res => {
+        this.results = res.data.results.slice();
+        console.log(this.results);
+      });
+      // this.searchBar = element;
+      console.log(this.searchBar);
+    },
+  },
+  computed:{
+
+  },
 }
 </script>
 
