94592f54961541b346ef4c4e5b5d837a5b719bb4
andmalt <andrea.maltempi@gmail.com>
2021-10-01 19:14:04 +0200

    inserted header temporary

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
+  <main class="container-fluid">
+    <div class="row">
+      <div class="col-12 p-3" id="header">
+        <div class="row">
+          <div class="col-6">
+            <a href="#"><img src="../assets/logo.png" alt="bootflix-font"></a>
+          </div>
 
-  </div>
+          <div class="col-6 d-flex justify-content-center align-items-center">
+            <input type="text" placeholder="Search">
+            <button class="btn-primary">search</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12" id="main-contain">
+
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -14,6 +31,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import '../style/general.scss';
+
+#header{
+  background-color: black;
+}
+#main-contain{
+  background-color: gray;
+}
 
 
 </style>
