5baa046c41fcb0344da8acf734b54d0bed7f1ecb
andmalt <andrea.maltempi@gmail.com>
2021-10-02 11:16:26 +0200

    fixed search

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,13 +9,22 @@
 
           <div class="col-6 d-flex justify-content-center align-items-center">
             <input type="text" placeholder="Search" v-model="searchBar">
-            <button @click="search" class="btn-primary">search</button>
+            <button @click.prevent="search" class="btn-primary">search</button>
           </div>
         </div>
       </div>
 
-      <div class="col-12" id="main-contain">
-
+      <div class="col-12 p-5" id="main-contain">
+        <div class="row">
+          <div v-for="(element , index) in movie" :key="index" class="col-12 col-sm-6 col-lg-2">
+            <div class="card">
+              <h2>{{ element.title}}</h2>
+              <h3>{{ element.original_title }}</h3>
+              <h5> {{element.original_language}} </h5>
+              <h6>{{ element.vote_average }}</h6>
+            </div>           
+          </div>
+        </div>
       </div>
     </div>
   </main>
@@ -31,18 +40,21 @@ export default {
   data(){
     return{
       searchBar:'',
-      results:[],
+      movie:[],
     }
   },
   methods:{
     search(){
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=9d0595ce1c1db683468e3d616a3c1b98&query=lo+squalo')
-        .then(res => {
-        this.results = res.data.results.slice();
-        console.log(this.results);
+      axios.get('https://api.themoviedb.org/3/search/movie?', {
+          params: {
+            api_key: '9d0595ce1c1db683468e3d616a3c1b98',
+            query: this.searchBar,
+          } 
+        })
+        .then((res)=> {
+          this.movie = [...res.data.results];
+          console.log(this.movie);
       });
-      // this.searchBar = element;
-      console.log(this.searchBar);
     },
   },
   computed:{
@@ -61,6 +73,9 @@ export default {
 #main-contain{
   background-color: gray;
 }
-
+.card{
+  background-color: black;
+  color: white;
+}
 
 </style>
