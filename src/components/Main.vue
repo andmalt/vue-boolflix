a632379d8d66652c5218a48267db57bd04860c4e
andmalt <andrea.maltempi@gmail.com>
2021-10-02 17:16:45 +0200

    inserted img poster and vote filtered

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -16,31 +16,33 @@
 
       <div class="col-12 p-5" id="main-contain">
         <div class="row justify-content-center g-3">
-          <div v-for="(element , index) in movie" :key="index" class="col-12 col-sm-6 col-lg-2 my-3">
-            <div  class="card">
+          <div v-for="(element , index) in movie" :key="index" class="col-12 col-sm-6 col-lg-3 my-3">
+            <div  class="card p-2">
               <ul>
-                <li><h5>Titolo: {{ element.title}}</h5></li>
+                <li><img class="img-fluid poster mb-3" :src="filterImage(element.poster_path)" :alt="'poster '+ element.name"></li>
+                <li><h5>Titolo: {{ element.title }}</h5></li>
                 <li><p>Titolo originale: {{ element.original_title }}</p></li>
                 <li>
                   <img v-if="element.original_language == 'it'" src="../assets/Italy.svg.png" alt="bandiera Italiana">
                   <img v-else-if="element.original_language == 'en'" src="../assets/United_Kingdom.svg.png" alt="bandiera Regno Unito">
                 </li>
-                <li><p>Voto: {{ element.vote_average }}</p></li>
+                <li><p>Voto: {{ filterAverage(element.vote_average) }}</p></li>
               </ul>
             </div>           
           </div>
         </div>
         <div class="row justify-content-center g-3">
-          <div v-for="(element , index) in tv" :key="index" class="col-12 col-sm-6 col-lg-2 my-3">
-            <div  class="card">
+          <div v-for="(element , index) in tv" :key="index" class="col-12 col-sm-6 col-lg-3 my-3">
+            <div  class="card p-2">
               <ul>
-                <li><h5>Titolo: {{ element.name}}</h5></li>
+                <li><img class="img-fluid poster mb-3" :src="filterImage(element.poster_path)" :alt="'poster '+ element.name"></li>
+                <li><h5>Titolo: {{ element.name }}</h5></li>
                 <li><p>Titolo originale: {{ element.original_name }}</p></li>
                 <li>
                   <img v-if="element.original_language == 'it'" src="../assets/Italy.svg.png" alt="bandiera Italiana">
                   <img v-else-if="element.original_language == 'en'" src="../assets/United_Kingdom.svg.png" alt="bandiera Regno Unito">
                 </li>
-                <li><p>Voto: {{ element.vote_average }}</p></li>
+                <li><p>Voto: {{ filterAverage(element.vote_average) }}</p></li>
               </ul>
             </div>           
           </div>
@@ -63,6 +65,7 @@ export default {
       movie:[],
       tv:[],
       keyApi: '9d0595ce1c1db683468e3d616a3c1b98',
+      image: 'https://image.tmdb.org/t/p/original/',
     }
   },
   methods:{
@@ -75,7 +78,7 @@ export default {
         })
         .then((res)=> {
           this.movie = [...res.data.results];
-          // console.log(this.movie);
+          console.log(this.movie);
       });
       axios.get('https://api.themoviedb.org/3/search/tv?', {
           params: {
@@ -88,6 +91,12 @@ export default {
           console.log(this.tv);
       });
     },
+    filterImage(element){
+      return this.image + element;
+    },
+    filterAverage(element){
+      return parseInt(element / 2);
+    }
   },
   computed:{
 
@@ -108,13 +117,22 @@ export default {
 .card{
   background-color: black;
   color: white;
+  height: 38rem;
   & img{
     width: 25px;
   }
   & ul{
     list-style: none;
     padding: 0;
+
+    & li{
+
+      .poster{
+        width: 80%;
+      }
+    }
   }
 }
+
 
 </style>
