53e74d25cba8421fb6cb64222fb78f3b6770f6fe
andmalt <andrea.maltempi@gmail.com>
2021-10-03 15:13:24 +0200

    refixed cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,7 +12,7 @@
               <ul>
                 <li><h5> <strong>Title:</strong>  {{ element.title }}</h5></li>
                 <li><h6> <strong>Original title:</strong>  {{ element.original_title }}</h6></li>
-                <li><p>Genre: movie</p></li>
+                <li><p><strong>Genre: </strong>movie</p></li>
                 <li>
                   <img v-if="element.original_language == 'it'" src="../assets/Italy.svg.png" alt="bandiera Italiana">
                   <img v-else-if="element.original_language == 'en'" src="../assets/United_Kingdom.svg.png" alt="bandiera Regno Unito">
@@ -48,7 +48,7 @@
                   <i  class="fas fa-star"></i>
                 </li>
                 <li>
-                  <p><strong>Overview: </strong> {{ element.overview }} </p>
+                  <p><strong>Overview: </strong> <em>{{ element.overview }}</em></p>
                 </li>
               </ul>
             </div>           
@@ -64,13 +64,13 @@
 
             <div  class="card text-start p-3 d-flex">
               <ul>
-                <li><h5>Title: {{ element.name }}</h5></li>
-                <li><p>Original Title: {{ element.original_name }}</p></li>
+                <li><h5> <strong>Title:</strong> {{ element.name }}</h5></li>
+                <li><h6> <strong>Original title:</strong> {{ element.original_name }}</h6></li>
                 <li>
                   <img v-if="element.original_language == 'it'" src="../assets/Italy.svg.png" alt="bandiera Italiana">
                   <img v-else-if="element.original_language == 'en'" src="../assets/United_Kingdom.svg.png" alt="bandiera Regno Unito">
                 </li>
-                 <li><p>Genre: TV series</p></li>
+                 <li><p><strong>Genre: </strong>TV series</p></li>
                 <li class="d-flex align-items-center" v-if="filterAverage(element.vote_average) == 1">
                   <p><strong>Average: </strong></p>
                   <i  class="fas fa-star"></i>
@@ -102,7 +102,7 @@
                   <i  class="fas fa-star"></i>
                 </li>
                 <li>
-                  <p><strong>Overview: </strong> {{ element.overview }} </p>
+                  <p><strong>Overview: </strong> <em>{{ element.overview }}</em></p>
                 </li>
               </ul>
             </div> 
@@ -171,6 +171,9 @@ export default {
         width: 100%;
       }
     }
+  }
+  & .fa-star{
+    color: goldenrod;
   }
 }
 
